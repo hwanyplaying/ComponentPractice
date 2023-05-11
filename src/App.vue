9e1395ca698b9c56ad2d7ac953c4b0cbb3f433d9
Hwany <hwanyplaying@gmail.com>
2023-05-11 16:22:44 +0900

    form 요소에 활용이 좋은 controlled component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <check-box v-model="checked"></check-box>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import CheckBox from "@/components/CheckBox.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    CheckBox
+  },
+  data() {
+    return {
+      checked: false
+    }
+  },
+  /*created() {
+    fetchCheckBox()
+        .then(function (result) {
+          this.checkboxes = result.data;
+        })
+  }*/
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CheckBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="checkbox" :value="value" @click="toggleCheck"/>
+</template>
+
+<script>
+export default {
+  name: "CheckBox",
+  props: ['value'],
+  methods: {
+    toggleCheck() {
+      // 1번 emit 사용
+      this.$emit('input', !this.value);
+      console.log(this.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
